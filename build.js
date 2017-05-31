585af10402dc1ec9392f94f6c13185e1fa46a70c
skatpgusskat <skatpgusskat@naver.com>
2017-05-31 15:07:29 +0900

    Add build.js

---
+++ build.js
@@ -0,0 +1,12 @@
+const ProgressBar = require('progress');
+
+const bar = new ProgressBar('빌드 진행중... [:bar] :percent', { total: 30 });
+function tick() {
+  bar.tick(0.3);
+  if (bar.complete) {
+    console.log('\n빌드 완료\n');
+    clearInterval(timer);
+  }
+}
+const timer = setInterval(tick, 1000);
+tick();
