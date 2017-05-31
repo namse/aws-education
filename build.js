677deba4e91f84f48c0e94936427897cae8108b4
skatpgusskat <skatpgusskat@naver.com>
2017-05-31 15:07:47 +0900

    Move todo files

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const ProgressBar = require('progress');
 
 const bar = new ProgressBar('빌드 진행중... [:bar] :percent', { total: 30 });
 function tick() {
-  bar.tick(0.3);
+  bar.tick(0.5);
   if (bar.complete) {
     console.log('\n빌드 완료\n');
     clearInterval(timer);
